265004e6e73642e0fa73267aa6ef905b5f46be19
Joel Schoolnik <joelschoolnik@JoelSchoolniks-13MBP-0012.local>
2017-03-13 22:45:13 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: 'localhost' });
+
+
+server.register(require('inert'), (err) => {
+
+    if (err) throw err;
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./index.html');
+        }
+    });
+});
+
+
+server.start((err) => {
+    if (err) throw err;
+    console.log(`Server running at: ${server.info.uri}`);
+});
